4322e4ae4e27200e7da987a11588cea3ff004e3b
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-01-07 18:15:50 +0100

    setup the coordination to be the root controller

---
+++ CoordinatorPattern/Coordinator.swift
@@ -0,0 +1,29 @@
+//
+//  Coordinator.swift
+//  CoordinatorPattern
+//
+//  Created by waheedCodes on 07/01/2021.
+//
+
+import Foundation
+import UIKit
+
+enum Event {
+    case didTapButton
+}
+
+protocol Coordinator {
+    var navigationController: UINavigationController? { get set }
+    
+    /// Tells the coordinator something happened...handle it.
+    func eventOccurred(with type: Event)
+    
+    /// A way to start the coordinator
+    func start()
+}
+
+// MARK: - A reference for the VC to the coordinator to relay events
+
+protocol Coordinating {
+    var coordinator: Coordinator? { get set }
+}
